b3ffdf4b1055e9d9122a42deb1d13e9b4f220700
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-02-01 11:21:41 +0100

    ..

--- TechNote_System/server/server.js
+++ TechNote_System/server/server.js
@@ -2,25 +2,25 @@ const express = require('express')
 const colors = require('colors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const PORT = process.env.PORT || 8080;
 
 // reat Object 
-const app = express()
+// const app = express()
 
 // Middlewares 
-app.us(express.json())
-app.use(morgan('dev'))
+// app.us(express.json())
+// app.use(morgan('dev'))
 
 // Routes 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: 'server running',
-    })
-})
+// app.get('/', (req, res) => {
+//     res.status(200).send({
+//         message: 'server running',
+//     })
+// })
 
 // Port 
-const port = process.env.PORT || 8080
 
 // Listener Port 
-app.listen(port, () => {
-    console.log(`Server Running in ${process.env.NODE_MODE} Moding `)
-})
+// app.listen(port, () => {
+//     console.log(`Server Running in ${process.env.NODE_MODE} Moding `)
+// })
